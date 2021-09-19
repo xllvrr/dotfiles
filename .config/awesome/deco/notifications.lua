9476fa1fc6524b494eb6d2a08be341be2891d1e0
xllvr <dtsa.tan@gmail.com>
2021-09-19 23:30:09 +0800

    Changes to notifications

--- .config/awesome/deco/notifications.lua
+++ .config/awesome/deco/notifications.lua
@@ -9,8 +9,8 @@ nconf.defaults.border_width = 3
 nconf.defaults.margin = 16
 nconf.defaults.text = "Boo!"
 nconf.defaults.timeout = 3
-nconf.padding = 8
-nconf.spacing = 8
+nconf.padding = 6
+nconf.spacing = 6
 nconf.opacity = .7
 
 -- Critical alert
@@ -26,4 +26,4 @@ nconf.presets.normal.bg = "#222222"
 nconf.presets.normal.fg = "#ffffff"
 
 -- Icon size
-nconf.defaults.icon_size = 16
+nconf.defaults.icon_size = 64
