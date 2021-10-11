af8cffcac3f5fd2435dfc64a7b7e611918af5e6c
xllvr <dtsa.tan@gmail.com>
2021-10-11 13:16:45 +0800

    Vimwiki reduced number of subwikis

--- .config/nvim/lua/plugin/vimwiki.lua
+++ .config/nvim/lua/plugin/vimwiki.lua
@@ -1,17 +1,9 @@
 -- Vimwiki
 vim.cmd([[
 if $HOSTNAME == 'archmain'
-    let g:vimwiki_list = [{'path': '/mnt/media/VimWiki','syntax': 'markdown', 'ext': '.md'},
-                \ {'path': '/mnt/media/VimWiki/Christian', 'syntax': 'markdown', 'ext':'md'},
-                \ {'path': '/mnt/media/VimWiki/DnD', 'syntax': 'markdown', 'ext':'md'},
-                \ {'path': '/mnt/media/VimWiki/Personal', 'syntax': 'markdown', 'ext':'md'},
-                \ {'path': '/mnt/media/VimWiki/Projects', 'syntax': 'markdown', 'ext':'md'}]
+    let g:vimwiki_list = [{'path': '/mnt/media/VimWiki','syntax': 'markdown', 'ext': '.md'}]
 elseif $HOSTNAME == 'archthink'
-    let g:vimwiki_list = [{'path': '~/Documents/Vimwiki','syntax': 'markdown', 'ext': '.md'},
-                \ {'path': '~/Documents/Vimwiki/Christian', 'syntax': 'markdown', 'ext':'md'},
-                \ {'path': '~/Documents/Vimwiki/DnD', 'syntax': 'markdown', 'ext':'md'},
-                \ {'path': '~/Documents/Vimwiki/Personal', 'syntax': 'markdown', 'ext':'md'},
-                \ {'path': '~/Documents/Vimwiki/Projects', 'syntax': 'markdown', 'ext':'md'}]
+    let g:vimwiki_list = [{'path': '~/Documents/Vimwiki','syntax': 'markdown', 'ext': '.md'}]
 endif
 ]])
 vim.g.vimwiki_folding = 'expr'
