cd71c269d21c372b42590685a0738adb7d1c80a7
xllvr <dtsa.tan@gmail.com>
2021-10-28 15:04:25 +0800

    Removed Last Epoch Rules

--- .config/awesome/main/rules.lua
+++ .config/awesome/main/rules.lua
@@ -73,10 +73,7 @@ function _M.get(clientkeys, clientbuttons)
       }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
-    
+    -- Map certain applications to certain tags
     { rule = { class = "Thunderbird" },
       properties = { tag = "" }
     },
@@ -99,10 +96,6 @@ function _M.get(clientkeys, clientbuttons)
 
     { rule = { class = "Lutris" },
       properties = { tag = "" }
-    },
-
-    { rule = {class = "Last Epoch.x86_64"},
-      properties = { tag = "", floating = true }
     },
 
   }
