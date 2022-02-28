cdaa43fc8b361b773fc5761be3afc41d910493ea
xllvr <dtsa.tan@gmail.com>
2022-02-28 13:02:28 +0800

    Rules for Floating

--- .config/awesome/main/rules.lua
+++ .config/awesome/main/rules.lua
@@ -17,95 +17,99 @@ function custom_focus_filter(c)
 end
 
 function _M.get(clientkeys, clientbuttons)
-  local rules = {
+    local rules = {
 
-    -- All clients will match this rule.
+        -- All clients will match this rule.
     { rule = { },
-      properties = {
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal,
-        focus     = custom_focus_filter,
-        raise     = true,
-        keys      = clientkeys,
-        buttons   = clientbuttons,
-        screen    = awful.screen.preferred,
-        placement = awful.placement.no_overlap+awful.placement.no_offscreen,
-        maximized_horizontal = false,
-        maximized_vertical = false,
-        maximized = false,
-      }
-    },
+            properties = {
+                border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus     = custom_focus_filter,
+            raise     = true,
+            keys      = clientkeys,
+            buttons   = clientbuttons,
+            screen    = awful.screen.preferred,
+            placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+            maximized_horizontal = false,
+            maximized_vertical = false,
+            maximized = false,
+            }
+            },
 
-    -- Floating clients.
+        -- Floating clients.
     { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
+            instance = {
+                "DTA",  -- Firefox addon DownThemAll.
+                "copyq",  -- Includes session name in class.
+                "pinentry",
+                },
+            class = {
+                "Arandr",
+                "Blueman-manager",
+                "Gpick",
+                "Gimp",
+                "Darktable",
+                "Kruler",
+                "MessageWin",  -- kalarm.
+                "Sxiv",
+                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+                "Wpa_gui",
+                "veromix",
+                "xtightvncviewer",
+                "calfjackhost",
+                "QjackCtl",
+                "Pavucontrol",
+            },
+            -- Note that the name property shown in xprop might be set slightly after creation of the client
+            -- and the name shown there might not match defined rules here.
+            name = {
+                "Event Tester",  -- xev.
+            "VimWiki" -- Vimwiki
+            },
+            role = {
+                "AlarmWindow",  -- Thunderbird's calendar.
+                "ConfigManager",  -- Thunderbird's about:config.
+                "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            }
         },
-        class = {
-          "Arandr",
-          "Blueman-manager",
-          "Gpick",
-          "Gimp",
-          "Darktable",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-          "Wpa_gui",
-          "veromix",
-          "xtightvncviewer"},
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          "Event Tester",  -- xev.
-          "VimWiki" -- Vimwiki
+            properties = { 
+                floating = true
+            }
         },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "ConfigManager",  -- Thunderbird's about:config.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      },
-      properties = { 
-        floating = true
-      }
-    },
 
-    -- Map certain applications to certain tags
+        -- Map certain applications to certain tags
     { rule = { class = "Thunderbird" },
-      properties = { tag = "" }
-    },
+            properties = { tag = "" }
+        },
 
     { rule = { instance = "discord" },
-      properties = { tag = "" }
-    },
+            properties = { tag = "" }
+        },
 
     { rule = { instance = "zoom" },
-      properties = { tag = "" }
-    },
+            properties = { tag = "" }
+        },
 
     { rule = { class = "Darktable" },
-      properties = { tag = "", fullscreen = true }
-    },
+            properties = { tag = "", fullscreen = true }
+        },
 
     { rule = { class = "Gimp" },
-      properties = { tag = "", floating = true }
-    },
+            properties = { tag = "", floating = true }
+        },
 
     { rule = { class = "Lutris" },
-      properties = { tag = "" }
-    },
+            properties = { tag = "" }
+        },
 
-  }
+    }
 
-  return rules
+    return rules
 end
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 return setmetatable(
-  {}, 
-  { __call = function(_, ...) return _M.get(...) end }
+{}, 
+{ __call = function(_, ...) return _M.get(...) end }
 )
