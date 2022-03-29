e664881aa4810b6cd4a23620ef95d5bdc5809b61
xllvr <dtsa.tan@gmail.com>
2022-03-29 11:27:48 +0800

    New Python Template

--- .config/nvim/templates/skeleton.py
+++ .config/nvim/templates/skeleton.py
@@ -5,3 +5,14 @@
 Author: Xllvr
 Date: [Add date here]
 """
+
+import pandas as pd
+import numpy as np
+import cufflinks as cf
+import chart_studio.plotly as py
+import seaborn as sns
+import plotly.express as px
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+cf.go_offline()
