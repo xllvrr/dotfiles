f0fbfc7263a953aa2278cfa877ad22fccb2e5e9b
xllvr <dtsa.tan@gmail.com>
2022-04-24 12:44:45 +0800

    New Plugins

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -134,6 +134,11 @@ _G.packer_plugins = {
     path = "/home/xllvr/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-dap"] = {
+    loaded = true,
+    path = "/home/xllvr/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
+  },
   ["nvim-ipy"] = {
     loaded = true,
     path = "/home/xllvr/.local/share/nvim/site/pack/packer/start/nvim-ipy",
