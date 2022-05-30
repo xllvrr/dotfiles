22cb2aa79c666651a4782b03a4c29006133ea77a
xllvr <dtsa.tan@gmail.com>
2022-05-30 12:25:58 +0800

    Edited Iron setup for update

--- .config/nvim/lua/plugin/iron.lua
+++ .config/nvim/lua/plugin/iron.lua
@@ -1,28 +1,39 @@
 ---- IRON-REPL ----------------------------------------------------------------
 local iron = require('iron')
 
-iron.core.add_repl_definitions {
-    python = {
-        venv_python = {
-            command =  "pipenv run ipython"
-        }
-    }
-}
-
-iron.core.set_config {
-    preferred = {
-        python = "ipython",
+iron.core.setup {
+    config = {
+        -- Highlights the last sent block with bold
+        highlight_last = "IronLastSent",
+        -- If iron should expose `<plug>(...)` mappings for the plugins
+        should_map_plug = false,
+        -- Whether a repl should be discarded or not
+        scratch_repl = true,
+        -- Automatically closes the repl window on process end
+        close_window_on_exit = true,
+        -- Your repl definitions come here
+        repl_definition = {
+            sh = {
+                command = { "zsh" }
+            },
+            python = require("iron.fts.python").ipython
+        },
+        -- Repl position
+        repl_open_cmd = require("iron.view").curry.right(function()
+            return math.ceil(vim.o.columns / 2)
+        end),
     },
-    repl_open_cmd = 'rightbelow vertical split'
+    -- Iron doesn't set keymaps by default anymore. Set them here
+    -- or use `should_map_plug = true` and map from you vim files
+    keymaps = {
+        send_motion = "<space>sc",
+        visual_send = "<space>sv",
+        send_line = "<space>sl",
+        repeat_cmd = "<space>s.",
+        cr = "<space>s<cr>",
+        interrupt = "<space>s<space>",
+        exit = "<space>sq",
+        clear = "<space>cl",
+    }
+
 }
-
--- Mappings
-local map = vim.api.nvim_set_keymap
-local options = { noremap = false }
-map('v', ',', '<Plug>(iron-visual-send)', options)
-map('n', ',', '<Plug>(iron-send-line)', options)
-map('', '<localleader>ir', ':IronRepl<CR>', options)
-map('', '<localleader>iq', '<Plug>(iron-exit)', options)
-map('', '<localleader>ic', '<Plug>(iron-clear)', options)
-
-vim.g.iron_map_extended = 0
