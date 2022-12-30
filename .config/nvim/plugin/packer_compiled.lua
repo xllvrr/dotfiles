8a2ebe4c9ac4ac3ee0ef68408f39717e09d21c79
xllvrr <dtsa.tan@gmail.com>
2022-12-30 18:23:37 +0800

    Archiving Configs

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -279,7 +279,13 @@ time([[Defining packer_plugins]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file UpdateRemotePlugins lua require("packer.load")({'semshi'}, { cmd = "UpdateRemotePlugins", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.api.nvim_create_user_command, 'UpdateRemotePlugins', function(cmdargs)
+          require('packer.load')({'semshi'}, { cmd = 'UpdateRemotePlugins', l1 = cmdargs.line1, l2 = cmdargs.line2, bang = cmdargs.bang, args = cmdargs.args, mods = cmdargs.mods }, _G.packer_plugins)
+        end,
+        {nargs = '*', range = true, bang = true, complete = function()
+          require('packer.load')({'semshi'}, { cmd = 'UpdateRemotePlugins' }, _G.packer_plugins)
+          return vim.fn.getcompletion('UpdateRemotePlugins ', 'cmdline')
+      end})
 time([[Defining lazy-load commands]], false)
 
 
